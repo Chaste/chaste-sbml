3a4e2f1c8192e1fd50463eeccca5e7df5a39f85f
Kwabena N Amponsah <kwabenantim@yahoo.com>
2025-01-20 13:16:58 +0000

    #3 Rename jinja templates

--- chaste_codegen_sbml/templates/common/hpp/base.hpp
+++ chaste_codegen_sbml/templates/common/hpp/base.hpp
@@ -1,12 +1,12 @@
 #ifndef {% filter upper %}{{class_name}}_HPP_{% endfilter %}
 #define {% filter upper %}{{class_name}}_HPP_{% endfilter %}
 
-{% include "common/hpp/comments.j2" %}
+{% include "common/hpp/blurb.hpp" %}
 
-{% include "common/hpp/includes.j2" %}
+{% include "common/hpp/includes.hpp" %}
 
-{% include "common/hpp/class_declaration.j2" %}
+{% include "common/hpp/class_declaration.hpp" %}
 
-{% include "common/hpp/serialization.j2" %}
+{% include "common/hpp/serialization.hpp" %}
 
 #endif // {% filter upper %}{{class_name}}_HPP_{% endfilter %}
